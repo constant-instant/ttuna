884db50c528fdc7e7cfd0fc298b99a4965706ef5
constant-instant <116035014+constant-instant@users.noreply.github.com>
2023-06-19 19:43:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict"
 
 // Analyser and animation
-var barColor = "#fc89ac"//"rgb(100,255,100)";
+var barColor = "#fc89ac";
 var context, source, analyser, 
-    frequencies, barWidth,
-	barHeight,lastNote = 0;
+	frequencies, barWidth, delta,
+	barHeight, prevTheta = 0, lastNote = 0;
 var display = document.getElementById("display");
 var frq = document.getElementById("frequency");
 
@@ -15,6 +15,11 @@ var c = document.getElementById("canvas");
 var vis = document.getElementById("visualization");
 var ctx = c.getContext("2d");
 var ctxVis = vis.getContext("2d");
+
+
+var angleStep = Math.PI / 9;
+var startAngle = Math.PI / 3;
+var endAngle = 2 * Math.PI / 3;
 
 var mobile, dim, ballRad, ringWidth, 
 	ox, oy, visRadius, fontSize;
@@ -40,7 +45,10 @@ function initValues() {
 	ox = w / 2;
 	oy = h / 2;
 	dim = Math.min( h, w );
+	ballRad = mobile ? dim * .015 : dim * .0075;
+	meanHeight = mobile ? dim * .25 : dim * .17;
 	visRadius = dim * .003;
+	ringWidth = 1//dim * .001;
 	fontSize = mobile ? dim * .05 : dim * .04;
 	display.style.fontSize = fontSize * 2 + "px";
 }
@@ -59,8 +67,7 @@ function tune( stream ) {
 		new Float32Array(analyser.frequencyBinCount);
 	anim();
 }
-var sum;
-//////////////******************
+var meanHeight, sum;
 function anim() {
 	//Pitch recognition / note display
 	var timeDomain = new Uint8Array(analyser.fftSize);
@@ -74,14 +81,17 @@ function anim() {
 	var pitch = WAD.autoCorrelate(timeDomain, context.sampleRate);
 	var noteName = WAD.noteFromPitch( pitch ) || lastNote;
 	// > > >
+
+
 	
 	//Visualization
 	vis.width = vis.width;
 	var freq, barHeight, barWidth;
+	var sum = 0;
 	var len = frequencies.length;
 	var start = parseInt(len * .1);
-	var end = parseInt(len * .4);
-	var step = 2 * Math.PI / 180;
+	var end = parseInt(len * .5);
+	var step = 2 * Math.PI / ( end - start );
 	analyser.getFloatFrequencyData(frequencies);
 	for(var i  = start; i < end; i++) {
 		freq = dim / 4 + frequencies[i];
@@ -93,7 +103,10 @@ function anim() {
 		ctxVis.rotate(step);
 		ctxVis.translate(-ox, -oy); 
 		ctxVis.fillRect(ox, oy, barWidth, barHeight );
+		sum += barHeight;
 	}
+
+	meanHeight = Math.abs(sum / (end - start));
 
 	// Display
 	c.width = c.width;
@@ -102,6 +115,45 @@ function anim() {
 		frq.innerHTML = Math.round(pitch) + " Hz";
 	}
 	lastNote = noteName;
-	////////////////************
+	
+	
+	delta = WAD.getDelta();
+	showDelta();
 	window.requestAnimationFrame(anim);
 }
+
+function showDelta() {
+	smoothAnim();
+}
+
+
+function smoothAnim() {
+	move();
+	function move() {
+		// Cursor ball
+		var step = .01;
+		var arc = ( endAngle - startAngle ) / 2;
+		var theta = Math.PI / 2 - 2 * delta * arc;
+		var sign = theta - prevTheta;
+		prevTheta += step * sign;
+		var diff = theta - prevTheta;
+		var bx = ox + Math.cos(prevTheta) * meanHeight;
+		var by = oy - Math.sin(prevTheta) * meanHeight;
+		var rad = ballRad;
+		ctx.fillStyle = "#000";
+		ctx.moveTo(bx, by);
+		ctx.beginPath();
+		ctx.ellipse( bx, by, rad, rad,
+					 0, 0, 2 * Math.PI);//шарик
+		ctx.moveTo(ox, oy);
+		ctx.ellipse(ox, oy, meanHeight, meanHeight,
+					0,  - prevTheta - .02,  -prevTheta + .02);//палочка
+		ctx.fill();
+
+		
+		if( Math.abs(diff) > step ) {
+			window.requestAnimationFrame(move);
+		}
+	}
+}
+
